47b989c923a5547edac0c9bd6e582b0a57788c53
Anny Beatriz <160553862+nomeanny@users.noreply.github.com>
2024-10-30 15:03:21 -0300

    Add files via upload

---
+++ exetable.lua
@@ -0,0 +1,9 @@
+local livro = {
+    titulo = "E assim que acaba",
+    autora = "Collen Hoover",
+    ano = 2016
+}
+
+print("Titulo: " .. livro.titulo)
+print("Autora: " .. livro.autora)
+print("Ano: " .. livro.ano)
