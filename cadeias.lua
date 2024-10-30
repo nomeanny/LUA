a818d93a92e3ade4d112fe6b92eec5417a21b249
Anny Beatriz <160553862+nomeanny@users.noreply.github.com>
2024-10-30 15:05:25 -0300

    Add files via upload

---
+++ cadeias.lua
@@ -0,0 +1,11 @@
+local str1 = 'Ola, Mundo!'
+local str2 = 'Lua'
+print(str1)
+print(str2)
+
+local nome = "Joao"
+local saudacao = "Ola, " .. nome
+print(saudacao)
+
+local comprimento = #str1
+print(comprimento)
