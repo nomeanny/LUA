4dd3e7af55e060a3a73c01ce580ad24e0e5f7acf
Anny Beatriz <160553862+nomeanny@users.noreply.github.com>
2024-11-05 15:20:12 -0300

    Add files via upload

---
+++ exeAN.lua
@@ -0,0 +1,10 @@
+function configurarCarro(modelo, ano, cor)
+    print("Modelo: " .. modelo)
+    print("Ano: " .. ano)
+    print("Cor: " .. cor)
+end
+
+local modelo = "Vectra"
+local ano = 2000
+local cor = "Azul"
+configurarCarro(modelo, ano, cor)
